eac37a1f8bfe0544afd35541f01533cced2eb827
hendriksen <gerrit.hendriksen@deltares.nl>
2023-02-22 09:40:20 +0100

    extra debug

--- processes/wps_bodem_boreholes.py
+++ processes/wps_bodem_boreholes.py
@@ -78,6 +78,10 @@ class BodemBoreholePlots(Process):
             locid = request.inputs["locationid"][0].data.split(' ')[0]            
            
             #db connection params
+            if os.path.exists('../config/bodembeweging.txt'):
+                print('pad naar configfile gevonden')
+            else:
+                print('misschien toch beter een ander pad gebruiken')
             db_config = os.path.join(dirname,'../config/bodembeweging.txt')
             PLOTS_DIR, APACHE_DIR = readConfig()
                        
